1f3f1302e48a610018d724dfcfda8adf404901fe
KynRy <150273047+KynRy@users.noreply.github.com>
2023-11-08 09:24:39 -0700

    Update ClearScanApp2.ps1

--- ClearScanApp2.ps1
+++ ClearScanApp2.ps1
@@ -1,11 +1,11 @@
-# Gracefully stop the scan.exe process
+# Stop the scanapp.exe process and any other windows
 Get-Process -Name scanapp* | Stop-Process -Force
 
 # Clear the contents of %appdata%\scan2
 Write-Host "Clearing contents of %appdata%\ScanApp2..."
-Remove-Item -Path "$env:APPDATA\ScanApp2\*" -Recurse -Force
+Remove-Item -Path "$env:C:\Users\Admin\AppData\Roaming\ScanApp2\*" -Recurse -Force
 
-# Restart the scan.exe process
+# Restart the scanapp.exe process
 Write-Host "Restarting scanapp.exe..."
 Start-Process -FilePath "C:\Program Files (x86)\ADOT_Scan\scanapp.exe"
 
