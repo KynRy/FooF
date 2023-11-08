336a7d48972cd675294ba4f05d8206f78f586570
KynRy <150273047+KynRy@users.noreply.github.com>
2023-11-08 11:30:45 -0700

    Update ClearScanApp2.ps1
    pre-pre-final

--- ClearScanApp2.ps1
+++ ClearScanApp2.ps1
@@ -25,42 +25,36 @@ if ($currentUser) {
         Remove-Item -Path $folderToDelete -Recurse -Force -Verbose
     } else {
         Write-Host "The folder $folderToDelete does not exist for the current user."
-    }
 } else {
     Write-Host "Unable to determine the current user."
 }
 
-# Define the path to the scanapp.exe executable
-$scanAppExePath = "C:\Program Files (x86)\ADOT_Scan\scanapp.exe"
+# Get the current logged-in user using WMI
+$LoggedUser = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
 
-# Check if "scanapp.exe" process is running
-$processName = "scanapp.exe"
-$runningProcess = Get-Process -Name $processName -ErrorAction SilentlyContinue
-
-if ($runningProcess) {
-    Write-Host "The $processName process is already running."
+# Check if the username is in the DOMAIN\Username format
+if ($LoggedUser -match '^(?<Domain>[\w\d]+)\\(?<Username>.+)$') {
+    $Domain = $Matches['Domain']
+    $Username = $Matches['Username']
 } else {
-    Write-Host "The $processName process is not running. Attempting to start it..."
-
-    # Define the task action to run scanapp.exe
-    $taskAction = New-ScheduledTaskAction -Execute "$scanAppExePath"
-
-    # Register the scheduled task to run as the currently logged-in user
-    $taskTrigger = New-ScheduledTaskTrigger -AtStartup
-    $taskUser = $currentUser
-
-    Register-ScheduledTask -TaskName "StartScanAppTask" -Action $taskAction -Trigger $taskTrigger -User $taskUser
-
-    # Start the scheduled task
-    Start-ScheduledTask -TaskName "StartScanAppTask"
-
-    Write-Host "The $processName process is now starting for the current user."
-
-    # Sleep for a while to give time for the task to run (you can adjust this time)
-    Start-Sleep -Seconds 10
-
-    # Unregister the scheduled task
-    Unregister-ScheduledTask -TaskName "StartScanAppTask" -Confirm:$false
-
-    Write-Host "The scheduled task has been deleted."
+    $Domain = $null
+    $Username = $LoggedUser
 }
+
+# Define the path to the scanapp.exe
+$ProgramPath = "C:\Program Files (x86)\ADOT_Scan\scanapp.exe"
+
+# Check if the program file exists
+if (Test-Path $ProgramPath -PathType Leaf) {
+    # Start the program with the current user's credentials
+    if ($Domain -eq $null) {
+        # Local user
+        Start-Process $ProgramPath
+    } else {
+        # Domain user
+        Start-Process -Credential "$Domain\$Username" -FilePath $ProgramPath
+    }
+} else {
+    Write-Host "The program file does not exist at $ProgramPath."
+}
+
