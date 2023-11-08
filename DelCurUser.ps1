1f18db18cf6ae709b9351cefa34c4fda3eb410a1
KynRy <150273047+KynRy@users.noreply.github.com>
2023-11-08 10:33:15 -0700

    Update DelCurUser.ps1
    v4

--- DelCurUser.ps1
+++ DelCurUser.ps1
@@ -1,7 +1,10 @@
-# Get the username of the currently logged-on user from the registry
-$currentUser = (Get-ItemProperty -Path "HKCU:\Volatile Environment\").UserName
+# Get the username of the currently logged-on user using WMI
+$currentUser = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
 
 if ($currentUser) {
+    # Extract the username from the full format (DOMAIN\Username)
+    $currentUser = $currentUser -replace '.*\\'
+
     # Construct the path to the user's AppData folder
     $folderToDelete = "C:\Users\$currentUser\AppData\Roaming\ScanApp2"
 
